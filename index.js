3c7760c7e9709fa373ef3980f017a1adc4db6382
vizzlearn@gmail.com <vizzlearn@gmail.com>
2019-02-26 23:58:32 +0700

    chore: completting fetch weather API

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 let request = require('request');
 
-let apiKey = '08b43a8f22b2b7109b3736788b4af11b';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let apiKey = 'b6907d289e10d714a6e88b30761fae22';
 
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-    console.log('body:');
-    console.log(JSON.parse(body));
-  }
-});
+function fetchWeatherAPI(url) {
+  return request(url, function (err, response, body) {
+    if(err){
+      console.log('error:', err);
+    } else {
+      console.log('body:');
+      console.log(JSON.parse(body));
+    }
+  });
+}
+
+function getWeatherByLocZip(arrayOfLocationZip) {
+  let weatherFetchList = [];
+
+  arrayOfLocationZip.forEach(function (val) {
+    let city = val.location;
+    let zip = val.zip;
+    let urlByCity = `https://samples.openweathermap.org/data/2.5/forecast?q=`+city+`&appid=`+apiKey;
+    let urlByZip = `https://samples.openweathermap.org/data/2.5/forecast?zip=`+zip+`&appid=`+apiKey;
+    weatherFetchList.push(fetchWeatherAPI(urlByCity));
+    weatherFetchList.push(fetchWeatherAPI(urlByZip));
+  })
+
+  Promise.all(weatherFetchList).then(function(values) {
+    console.log(values);
+  });
+}
+
+let paramData = [{location: 'portland', zip: '99524'}, {location: 'Denver', zip: '80239'}];
+
+getWeatherByLocZip(paramData);
